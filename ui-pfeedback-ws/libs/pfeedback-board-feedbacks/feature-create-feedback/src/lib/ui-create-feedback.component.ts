547b79d61459da07f536a50015370f86e122eee4
Ignacio Fontalvo <ignacioa@backbase.com>
2022-07-16 15:40:58 +0200

    feat: creating ui create feedback

--- ui-pfeedback-ws/libs/pfeedback-board-feedbacks/feature-create-feedback/src/lib/ui-create-feedback.component.ts
+++ ui-pfeedback-ws/libs/pfeedback-board-feedbacks/feature-create-feedback/src/lib/ui-create-feedback.component.ts
@@ -1,39 +1,94 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
+import { UIDropdownModule } from './ui-dropdown.component';
 
 @Component({
   selector: 'app-ui-create-feedback',
   template: `
-    <div>
-      <h1>Create New Feedback</h1>
-      <div>
-        <label>Feedback Title</label>
-        <p>Add a short, descriptive headline</p>
-        <!-- input here -->
+    <div class="rounded-lg p-10 bg-white">
+      <h1 class="text-2xl font-bold tracking-tight text-indigo-900 mb-10">
+        Create New Feedback
+      </h1>
+      <div class="mb-6">
+        <label for="title">
+          <span class="text-sm font-bold tracking-tight text-indigo-900"
+            >Feedback Title</span
+          >
+          <p class="text-slate-600 text-base mb-4">
+            Add a short, descriptive headline
+          </p>
+        </label>
+        <input
+          type="text"
+          id="title"
+          class="h-12 w-full rounded bg-slate-100"
+        />
       </div>
-      <div>
-        <label>Category</label>
-        <p>Choose a category for your feedback</p>
-        <!-- Select drop down here -->
+
+      <div class="mb-6">
+        <label for="category">
+          <span class="text-sm font-bold tracking-tight text-indigo-900"
+            >Category</span
+          >
+          <p class="text-slate-600 text-base mb-4">
+            Choose a category for your feedback
+          </p>
+        </label>
+        <app-ui-dropdown
+          [options]="options"
+          (selectedItem)="onSelectCategory($event)"
+        ></app-ui-dropdown>
       </div>
-      <div>
-        <label>Feedback Detail</label>
-        <p>
-          Include any specific comments on what should be improved, added, etc.
-        </p>
+
+      <div class="mb-8">
+        <label for="description">
+          <span class="text-sm font-bold tracking-tight text-indigo-900"
+            >Feedback Detail</span
+          >
+          <p class="text-slate-600 text-base mb-4">
+            Include any specific comments on what should be improved, added,
+            etc.
+          </p>
+        </label>
         <!-- Text area here -->
+        <textarea
+          id="description"
+          cols="30"
+          rows="10"
+          class="w-full rounded resize-none bg-slate-100"
+        ></textarea>
       </div>
-      <div>
-        <button>Cancel</button>
-        <button>Add Feedback</button>
+      <div class="flex justify-end">
+        <button
+          class="rounded-lg px-6 py-3 bg-indigo-900 text-white font-bold text-sm mr-4"
+        >
+          Cancel
+        </button>
+        <button
+          class="rounded-lg px-6 py-3 bg-fuchsia-700 text-white font-bold text-sm"
+        >
+          Add Feedback
+        </button>
       </div>
     </div>
   `,
 })
-export class UICreateFeedbackComponent {}
+export class UICreateFeedbackComponent {
+  options = [
+    { key: 'Option 1', label: 'Option 1' },
+    { key: 'Option 2', label: 'Option 2' },
+    { key: 'Option 3', label: 'Option 3' },
+  ];
+
+  selectedCategory = '';
+
+  onSelectCategory(value: string | number): void {
+    this.selectedCategory = value as string;
+  }
+}
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, UIDropdownModule],
   declarations: [UICreateFeedbackComponent],
   exports: [UICreateFeedbackComponent],
 })
---
+++ ui-pfeedback-ws/libs/pfeedback-board-feedbacks/feature-create-feedback/src/lib/ui-dropdown.component.ts
@@ -0,0 +1,75 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  NgModule,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-ui-dropdown',
+  template: ` <div class="relative">
+    <div
+      tabindex="0"
+      (click)="toggleOpen()"
+      class="h-12 w-full p-5 flex justify-between items-center rounded bg-slate-100 cursor-pointer"
+    >
+      <span>{{ selected ? selected : 'Pick One' }}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-chevron-up"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+        />
+      </svg>
+    </div>
+    <ul
+      *ngIf="open"
+      class="w-full absolute mt-4 rounded-lg top-full bg-white shadow-2xl"
+    >
+      <li
+        *ngFor="let option of options"
+        class="px-6 py-3 border-b border-indigo-900/10 cursor-pointer"
+        (click)="selectItem(option.key)"
+      >
+        {{ option.label }}
+      </li>
+    </ul>
+  </div>`,
+})
+export class UIDropdownComponent {
+  @Input()
+  options: { key: string; label: string | number }[] = [];
+
+  @Output()
+  selectedItem: EventEmitter<string | number> = new EventEmitter<
+    string | number
+  >();
+
+  selected: string | number = '';
+  open = false;
+
+  toggleOpen(): void {
+    this.open = !this.open;
+  }
+
+  selectItem(value: string | number): void {
+    this.selected = value;
+    this.selectedItem.emit(this.selected);
+    this.toggleOpen();
+  }
+}
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [UIDropdownComponent],
+  exports: [UIDropdownComponent],
+})
+export class UIDropdownModule {}
